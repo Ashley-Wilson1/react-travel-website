a3a714a7eed2c21acf922a945f14d3f96c3d417b
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-12-04 00:27:45 +0000

    Added content to footer

--- react-website-travel/src/App.js
+++ react-website-travel/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
+import Services from "./components/Pages/Services";
+import Products from "./components/Pages/Products";
+import SignUp from "./components/Pages/SignUp";
 
 function App() {
 	return (
@@ -11,6 +14,9 @@ function App() {
 				<NavBar />
 				<Routes>
 					<Route path="/" exact Component={Home} />
+					<Route path="/services" Component={Services} />
+					<Route path="/products" Component={Products} />
+					<Route path="/sign-up" Component={SignUp} />
 				</Routes>
 			</Router>
 		</>
